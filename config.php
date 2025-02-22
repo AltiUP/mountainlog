ba090f146c5450889b5c3d0771d932cea7f2d6f8
Christophe Gherardi <44464440+AltiUP@users.noreply.github.com>
2025-02-22 18:50:54 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Configuration de la base de données
 define('DB_SERVER', 'localhost'); // Adresse du serveur
-define('DB_USERNAME', 'AltiUP_mountainlog');    // Nom d'utilisateur MySQL
-define('DB_PASSWORD', 'O%F8xANXZikt6iKX');        // Mot de passe MySQL
-define('DB_NAME', 'AltiUP_mountainlog'); // Nom de la base de données
+define('DB_USERNAME', 'mountainlog');    // Nom d'utilisateur MySQL
+define('DB_PASSWORD', 'password');        // Mot de passe MySQL
+define('DB_NAME', 'mountainlog'); // Nom de la base de données
 
 // Connexion à la base de données
 $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
